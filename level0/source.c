222ff27682dd8948d00a7853b870de9aacbc10c2
Andy <AndyShih51@gmail.com>
2019-03-13 18:31:11 -0700

    Clear level0

---
+++ level0/source.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+# define SHELL "/bin/sh"
+
+int			main(int argc, char **argv)
+{
+	char	*exec_args[2];
+	gid_t	egid;
+	uid_t	euid;
+
+	if (atoi(argv[1]) == 423)
+	{
+		exec_args[0] = strdup(SHELL);
+		exec_args[1] = NULL;
+		egid = getegid();
+		euid = geteuid();
+		setresgid(egid, egid, egid);
+		setresuid(euid, euid, euid);
+		execv(SHELL, exec_args);
+	}
+	else
+	{
+		fwrite("No !\n", 1, 5, stderr);
+	}
+	return 0;
+}
