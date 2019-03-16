932aca1d0c30452c1bc5868de186071e7a73877c
Andy <AndyShih51@gmail.com>
2019-03-15 22:22:45 -0700

    Add level5 source.c and recreation instruction

---
+++ level5/source.c
@@ -0,0 +1,28 @@
+/*
+	source5.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void		o(void)
+{
+	system("/bin/sh");
+	_exit(1);
+}
+
+void 		n(void)
+{
+	char	buffer[512];
+
+	fgets(buffer, 512, stdin);
+	printf(buffer);
+	exit(1);
+}
+
+int			main(void)
+{
+	n();
+	return 0;
+}
