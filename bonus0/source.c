ba67977551042b3f50be506c455d91506e7c8571
Andy <AndyShih51@gmail.com>
2019-03-17 07:40:34 -0700

    Add bonus0 source.c and recreation instruction

---
+++ bonus0/source.c
@@ -0,0 +1,45 @@
+/*
+	bonus0.c
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+char				*p(char *a, char *delim)
+{
+	char			buffer[4096];
+
+	puts(delim);
+	read(STDIN_FILENO, buffer, 4096);
+	*strchr(buffer, '\n') = '\0';
+	return strncpy(a, buffer, 20);
+}
+
+char				*pp(char *buffer)
+{
+	char			b[20];
+	char			a[20];
+	unsigned int	len;
+
+	p(a, " - ");
+	p(b, " - ");
+
+	strcpy(buffer, a);
+
+	len = strlen(buffer);
+	buffer[len] = ' ';
+	buffer[len + 1] = '\0';
+
+	return strcat(buffer, b);
+}
+
+int					main(void)
+{
+	char			buffer[42];
+
+	pp(buffer);
+	puts(buffer);
+	return 0;
+}
