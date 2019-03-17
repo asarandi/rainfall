67aa1d49f7b227d9a23cc6acfcc85a4c57e9ed54
Alexandr Sarandi <asarandi@student.42.us.org>
2019-03-17 05:56:12 -0700

    bonus1 walkthrough edit and recreated source file

---
+++ bonus1/source.c
@@ -0,0 +1,24 @@
+/* compile: 	gcc -fno-stack-protector source.c 	*/
+
+#include <stdlib.h>	/* atoi */
+#include <unistd.h>	/* execl */
+#include <string.h>	/* memcpy */
+
+int main(int argc, char **argv)
+{
+	char	buf[40];
+	int	k;
+
+	k = atoi(argv[1]);
+	if (k < 10) {
+        k *= 4;
+		memcpy(buf, argv[2], k);
+		if (k == 0x574f4c46) {
+				execl("/bin/sh", "sh", NULL);
+		}
+
+	} else {
+		return 1;
+	}
+	return 0;
+}
